09c03d48dd4400ce6f85156b99e761c40892390a
bossadip78-dev <bossadip78@gmail.com>
2026-07-08 01:27:21 +0600

    Update config.py

--- config.py
+++ config.py
@@ -46,18 +46,18 @@ STRING5 = os.getenv("STRING_SESSION5", None)
 
 AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 
-START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/7q8bfg.jpg")
-PING_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/eehxb4.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
+START_IMG_URL = os.getenv("START_IMG_URL", "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg")
+PING_IMG_URL = "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg"
+PLAYLIST_IMG_URL = "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg"
+STATS_IMG_URL = "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg"
+TELEGRAM_AUDIO_URL = "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg"
+TELEGRAM_VIDEO_URL = "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg"
+STREAM_IMG_URL = "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg"
+SOUNCLOUD_IMG_URL = "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg"
+YOUTUBE_IMG_URL = "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://i.ibb.co.com/WvtNTSB8/photo-2026-07-08-01-25-15.jpg"
 
 BANNED_USERS = filters.user()
 adminlist = {}
